4f6488086ed5052b37d7a0964c2185e27eeb38ad
Ben <bsmith20681@gmail.com>
2026-03-30 10:00:08 -0400

    added yaml import to mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import yaml from '@rollup/plugin-yaml';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.secureliving.com',
+  vite: {
+    plugins: [yaml()],
+  },
   integrations: [
     mdx(),
     sitemap({
